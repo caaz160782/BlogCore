3f733367b7e63bcd45d3ed73fc1ed4f542fe919f
caaz <carlos.aguirre@it-seekers.com>
2025-05-07 13:45:16 -0600

    listado articulos

---
+++ BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,51 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _contenedorTrabajo.Articulo.GetAll(includeProperties: "Categoria");
+            return Json(new { data = allObj });
+        }
+
+       /* [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _contenedorTrabajo.Categoria.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error borrando categoria" });
+            }
+            _contenedorTrabajo.Categoria.Remove(objFromDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Se ha borrado la categoria" });
+        }*/
+
+
+        #endregion
+    }
+}
