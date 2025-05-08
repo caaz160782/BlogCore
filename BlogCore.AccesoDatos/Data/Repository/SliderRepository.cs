f9764df309bdc540141fd5e476232f78ab39669c
caaz <carlos.aguirre@it-seekers.com>
2025-05-08 14:42:23 -0600

    end crud slider

---
+++ BlogCore.AccesoDatos/Data/Repository/SliderRepository.cs
@@ -0,0 +1,32 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    internal class SliderRepository : Repository<Slider>, ISliderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public SliderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+           
+
+        public void Update(Slider slider)
+        {
+            var objFromDb = _db.Slider.FirstOrDefault(u => u.Id == slider.Id);
+            objFromDb.Nombre      = slider.Nombre;
+            objFromDb.estado = slider.estado;
+            objFromDb.UrlImagen   = slider.UrlImagen;
+           
+
+        }
+    }   
+    
+}
