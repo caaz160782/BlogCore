c884c49903dbb9a7f28150c165124069cb4cc565
Charles Aguirre <caaz160782@gmail.com>
2025-05-15 21:20:09 -0600

    funcionalidad de desbloquear y bloquear usuarios

---
+++ BlogCore.AccesoDatos/Data/Repository/UsuarioRepository.cs
@@ -0,0 +1,40 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public class UsuarioRepository : Repository<ApplicationUser>, IUsuarioRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        public UsuarioRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void BloquearUsuario(string IdUsuario)
+        {
+            var usuario = _db.ApplicationUser.FirstOrDefault(u => u.Id == IdUsuario);
+            if (usuario != null)
+            {
+                usuario.LockoutEnd = DateTime.Now.AddYears(1000);
+                _db.SaveChanges();
+            }
+        }
+        public void DesbloquearUsuario(string IdUsuario)
+        {
+            var usuario = _db.ApplicationUser.FirstOrDefault(u => u.Id == IdUsuario);
+            if (usuario != null)
+            {
+                usuario.LockoutEnd = null;
+                _db.SaveChanges();
+            }
+        }
+    }
+    
+}
---
+++ BlogCore/Areas/Admin/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuarioController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        
+        public UsuarioController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+         
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Aquí puedes obtener la lista de usuarios desde la base de datos
+            return View(_contenedorTrabajo.Usuario.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Bloquear(string id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            _contenedorTrabajo.Usuario.BloquearUsuario(id);
+            _contenedorTrabajo.Save();
+            return RedirectToAction("Index");   
+        }
+
+        [HttpGet]
+        public IActionResult Desbloquear(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _contenedorTrabajo.Usuario.DesbloquearUsuario(id);
+            _contenedorTrabajo.Save();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
